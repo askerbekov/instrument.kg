43919f8fe9bceee791ae58b18ea3ac74898d4f5d
TotSamiy2Pac <a.r.a.p.o.v@yandex.ru>
2023-11-13 17:09:57 +0600

    categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import categories from "./components/categories/categories";
 
 const App = () => {
+
   return (
     <>
-
+      {
+        categories.category.map(cat => {
+          return(
+            <ul>
+              <li>
+                {cat.category}
+                <ul>
+                  {
+                    cat.podcategoriy.map(podcat => {
+                      return(
+                        <li>{podcat}</li>
+                      )
+                    })
+                  }
+                </ul>
+              </li>
+            </ul>
+          )
+        })
+      }
     </>
   );
 };
