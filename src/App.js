da116df1729abf86b427a055b6e3f990131369ab
askerbekov <askerbekov.mederbek@gmail.com>
2023-12-03 04:21:13 +0600

    added page brends

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from "./pages/HomePage";
 import {Route, Routes} from "react-router-dom";
 import Categories from "./components/categories/categories";
 import CategoriesPage from "./pages/CategoriesPage";
+import BrandsPage from "./pages/BrandsPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path={'/'} element={<HomePage/>}/>
       <Route path={'/categories'} element={<CategoriesPage/>} />
+      <Route path={'/brands'} element={<BrandsPage/>} />
     </Routes>
   );
 };
