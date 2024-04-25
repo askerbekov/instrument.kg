461f6d5f62929d5abab91b446b8c1cfd2f4d6187
TotSamiy2Pac <a.r.a.p.o.v@yandex.ru>
2024-04-25 18:05:07 +0300

    refac adv

--- src/components/advertisement/Advertisement.jsx
+++ src/components/advertisement/Advertisement.jsx
@@ -4,10 +4,10 @@ import './advertisement.scss'
 const Advertisement = () => {
   return (
     <div className={'advertisement'}>
-      <div className={'advertisement-top'}>
+      <div className={'advertisement-top-box'}>
         <h2>Реклама рассрочки</h2>
       </div>
-      <div className="advertisement-bottom">
+      <div className="advertisement-bottom-box">
         <h2>Реклама программы лояльности</h2>
       </div>
     </div>
